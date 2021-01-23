e0f0952b691aa82874b2514dda0175fc960754e1
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2021-01-23 11:29:34 -0800

    envs now with versions

--- build-base.sh
+++ build-base.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TAG="us.gcr.io/artifacts-298104/base"
+TAG="us.gcr.io/artifacts-298104/base:v1"
 
 docker build -f base.dockerfile -t "${TAG}" .
 docker push "${TAG}"
--- build-java.sh
+++ build-java.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TAG="us.gcr.io/artifacts-298104/java"
+TAG="us.gcr.io/artifacts-298104/java:v1"
 
 docker build -f java.dockerfile -t "${TAG}" .
 docker push "${TAG}"
