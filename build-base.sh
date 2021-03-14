08e41bd1707cda73ee61aa3efa4aed5d14310eb3
German Malinovsky <german.malinovsky@gmail.com>
2021-03-14 23:52:44 +0100

    Added layers' minimizing, clean up and cache busting

--- build-base.sh
+++ build-base.sh
@@ -2,5 +2,5 @@
 
 TAG="us.gcr.io/artifacts-298104/base:v6"
 
-docker build -f base.dockerfile -t "${TAG}" .
+docker build --no-cache -f base.dockerfile -t "${TAG}" .
 docker push "${TAG}"
--- build-java.sh
+++ build-java.sh
@@ -2,5 +2,5 @@
 
 TAG="us.gcr.io/artifacts-298104/java:v13"
 
-docker build -f java.dockerfile -t "${TAG}" .
+docker build --no-cache -f java.dockerfile -t "${TAG}" .
 docker push "${TAG}"
