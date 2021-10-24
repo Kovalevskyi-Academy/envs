8bac6752a2f867aaa7db1397e5d09e1f541a5c27
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2021-10-24 14:06:31 -0700

    autorun.sh now has specific version of Zeus (not auto derive)

--- autorun.sh
+++ autorun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ZEUS_VERSION=$(curl -s https://api.github.com/repos/Kovalevskyi-Academy/Zeus/releases/latest | jq -r '.tag_name')
+ZEUS_VERSION="47"
 STR="export CLASSPATH=/lib/Zeus-${ZEUS_VERSION}.jar"
 printf "$STR" >> /root/.bashrc
 wget -nc https://github.com/Kovalevskyi-Academy/Zeus/releases/download/${ZEUS_VERSION}/Zeus-${ZEUS_VERSION}.jar -P /lib
